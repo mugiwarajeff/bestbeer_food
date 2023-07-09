55b991c90e9bc19b17a6c1beddd66d9cd5b6e336
jefferson <jeffersoncerqueira75@gmail.com>
2023-07-09 20:10:00 -0300

    .

--- src/app/features/Login/Login.tsx
+++ src/app/features/Login/Login.tsx
@@ -1,9 +1,23 @@
+import Input from "./Input/Input";
 import styles from "./Login.module.scss";
 
 export default function Login() {
-    return <div className={styles.login}>
-        <h2>
-            Login Page
-        </h2>
-    </div>;
+    return <section className={styles.login} >
+
+        <div className={styles["login__login-container"]}>
+            <h2>
+                Login no sistema
+            </h2>
+            <div>
+                <Input/>
+                <Input/>
+            </div>
+            <div className={styles["login__button-container"]}>
+                <div>button</div>
+                <h6>Esqueceu a senha?</h6>
+            </div>
+
+        </div>
+    </section>;
+
 }
