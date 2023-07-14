32b4fb9898a3e54fb736e9c73caa64e2fc9b0b12
Jefferson <jeffersoncerqueira75@gmail.com>
2023-07-14 17:39:40 -0300

    iniciando implantação de comandos de voz

--- src/app/features/Login/Input/Input.tsx
+++ src/app/features/Login/Input/Input.tsx
@@ -1,9 +1,16 @@
+import { IconType } from "react-icons/lib";
 import styles from "./Input.module.scss";
 
-export default function Input() {
+interface InputProps {
+    Icon: IconType
+}
+
+export default function Input({ Icon }: InputProps) {
 
     return <div className={styles["input-container"]}>
-        <div className={styles["input-container__icon"]}></div>
+        <div className={styles["input-container__icon"]}>
+            <Icon color="black" size="32px" />
+        </div>
         <input type="text" placeholder={"Usuário"} />
     </div>;
 }
--- src/app/features/Login/Login.tsx
+++ src/app/features/Login/Login.tsx
@@ -1,22 +1,24 @@
 import Input from "./Input/Input";
 import styles from "./Login.module.scss";
+import { FaRegUser } from "react-icons/fa";
+import { FiLock } from "react-icons/fi";
 
 export default function Login() {
     return <section className={styles.login} >
-
         <div className={styles["login__login-container"]}>
-            <h2>
-                Login no sistema
-            </h2>
-            <div>
-                <Input/>
-                <Input/>
+            <div className={styles["login__itens-container"]}>
+                <h2>
+                    Login no sistema
+                </h2>
+                <div>
+                    <Input Icon={FaRegUser} />
+                    <Input Icon={FiLock} />
+                </div>
+                <div className={styles["login__button-container"]}>
+                    <div>button</div>
+                    <h6>Esqueceu a senha?</h6>
+                </div>
             </div>
-            <div className={styles["login__button-container"]}>
-                <div>button</div>
-                <h6>Esqueceu a senha?</h6>
-            </div>
-
         </div>
     </section>;
 
