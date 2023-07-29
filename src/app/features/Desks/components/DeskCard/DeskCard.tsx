dd17b825dac4af1efa9ef0396c4a4b7063085b05
jefferson <jeffersoncerqueira75@gmail.com>
2023-07-29 11:08:00 -0300

    adding grid layout to desk feature

--- src/app/features/Desks/components/DeskCard/DeskCard.tsx
+++ src/app/features/Desks/components/DeskCard/DeskCard.tsx
@@ -1,5 +1,7 @@
 import { IDesk } from "../../interfaces/IDesk";
 import styles from "./DeskCard.module.scss";
+import { AiOutlineEye } from "react-icons/ai";
+import { FaMoneyBills } from "react-icons/fa6";
 
 interface DeskCardProps {
     desk: IDesk
@@ -8,6 +10,22 @@ interface DeskCardProps {
 export default function DeskCard(props: DeskCardProps){
 
     return <div className={styles.deskCard}>
-        
-    </div>;
+            <div className={styles.deskCard__itens}>
+                <div>
+                    <h1>
+                        {`Mesa ${props.desk.id}`}
+                    </h1>
+                    {props.desk.available ? 
+                        "Disponivel" : 
+                        "Indisponivel"}
+                </div>
+                <div className={styles.eyeIcon}>
+                <AiOutlineEye size={64}/>
+                </div>
+                <div className={styles.deskCard__itens__view_details}>
+                    <FaMoneyBills size={22}/>
+                    <a href="">Visualizar Conta</a>
+                </div>    
+            </div>
+        </div>;
 }
