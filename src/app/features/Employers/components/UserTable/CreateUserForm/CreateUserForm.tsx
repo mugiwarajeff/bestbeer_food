95dba8c611ae220665926a5e380de4b39b4ea0aa
Jefferson <jeffersoncerqueira75@gmail.com>
2023-10-11 09:25:47 -0300

    starting field validations

--- src/app/features/Employers/components/UserTable/CreateUserForm/CreateUserForm.tsx
+++ src/app/features/Employers/components/UserTable/CreateUserForm/CreateUserForm.tsx
@@ -1,9 +1,9 @@
 import useEmployers from "app/features/Employers/hooks/useEmployers";
 import { IEmpoyersService } from "app/features/Employers/interfaces/IEmployersService";
-import AxiosEmployerService from "app/features/Employers/services/AxiosEmployerService";
 import Form from "app/shared/components/Form/Form";
 import InputForm from "app/shared/components/Form/FormInput/FormInput";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 interface CreateUserForm {
     isOpen: boolean,
     onClose: () => void,
@@ -16,6 +16,8 @@ export default function CreateUserForm(props: CreateUserForm) {
     const employerService: IEmpoyersService = props.employerServiceInstance;
 
     const [employers, setEmployers] = useEmployers();
+    const { handleSubmit, register, formState } = useForm({ mode: "onSubmit" });
+    const { errors } = formState;
 
     const [user, setUser] = useState<string>("");
     const [name, setName] = useState<string>("");
@@ -35,9 +37,7 @@ export default function CreateUserForm(props: CreateUserForm) {
         setPassword("");
     }, [props.isOpen]);
 
-    const createHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
+    const createHandler = async () => {
         const newEmployer = await employerService.createEmployer({ user, name, cpf: cpf.replace(".", "").replace("-", ""), telefone: phone, email, role, password });
         if (newEmployer !== undefined) {
             setEmployers([...employers, newEmployer]);
@@ -46,13 +46,49 @@ export default function CreateUserForm(props: CreateUserForm) {
         props.onClose();
     };
 
-    return <Form isOpen={props.isOpen} title={"Criar Usuario"} onSubmit={createHandler} onCancel={props.onClose}>
-        <InputForm name={"Usuário"} value={user} onChange={event => setUser(event.target.value)} />
-        <InputForm name={"Nome"} value={name} onChange={event => setName(event.target.value)} />
-        <InputForm name={"CPF"} value={cpf} onChange={event => setCpf(event.target.value)} />
-        <InputForm name={"Telefone"} value={phone} onChange={event => setPhone(event.target.value)} />
-        <InputForm name={"E-mail"} value={email} onChange={event => setEmail(event.target.value)} />
-        <InputForm name={"Role"} value={role} onChange={event => setRole(event.target.value)} />
-        <InputForm name={"Password"} value={password} onChange={event => setPassword(event.target.value)} />
+    return <Form
+        isOpen={props.isOpen}
+        title={"Criar Usuario"}
+        onSubmit={handleSubmit(createHandler)}
+        onCancel={props.onClose}>
+        <InputForm
+            name={"Usuário"}
+            register={{ ...register("user", { required: "*Campo Obrigatório" }) }}
+            value={user}
+            onChange={event => setUser(event.target.value)}
+            errorState={errors.user} />
+        <InputForm
+            name={"Nome"}
+            value={name}
+            onChange={event => setName(event.target.value)}
+            register={{ ...register("name", { required: "*Campo Obrigatório" }) }}
+            errorState={errors.name} />
+        <InputForm
+            name={"CPF"}
+            value={cpf}
+            onChange={event => setCpf(event.target.value)}
+            register={{
+                ...register("cpf", {
+                    maxLength: { value: 11, message: "CPF inválido" },
+                    minLength: { value: 11, message: "CPF inválido" },
+                })
+            }}
+            errorState={errors.cpf} />
+        <InputForm
+            name={"Telefone"}
+            value={phone}
+            onChange={event => setPhone(event.target.value)} />
+        <InputForm
+            name={"E-mail"}
+            value={email}
+            onChange={event => setEmail(event.target.value)} />
+        <InputForm
+            name={"Role"}
+            value={role}
+            onChange={event => setRole(event.target.value)} />
+        <InputForm
+            name={"Password"}
+            value={password}
+            onChange={event => setPassword(event.target.value)} />
     </Form>;
 }
--- src/app/shared/components/Form/FormInput/FormInput.tsx
+++ src/app/shared/components/Form/FormInput/FormInput.tsx
@@ -1,18 +1,27 @@
+import { FieldError, FieldErrorsImpl, Merge, UseFormRegisterReturn } from "react-hook-form";
 import styles from "./FormInput.module.scss";
 
 interface FormInputProps {
     name: string,
     value: string | number,
     readonly?: boolean,
-    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined
+    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined,
+    register?: UseFormRegisterReturn,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    errorState?: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined,
 }
 
 export default function FormInput(props: FormInputProps) {
 
     return <div className={styles.formInput}>
         <div className={styles.deskFormInput}>
-            <label htmlFor={props.name}>{props.name}</label>
-            <input type={props.name} value={props.value} onChange={props.onChange} readOnly={props.readonly} />
+            <label htmlFor={props.name}>{props.name} <span>{props.errorState ? props.errorState.message?.toString() : ""}</span></label>
+            <input
+                type={props.name}
+                value={props.value}
+                onChange={props.onChange}
+                readOnly={props.readonly}
+                {...props.register} />
         </div>
     </div>;
 }
